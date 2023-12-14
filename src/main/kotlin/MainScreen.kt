2dac526ac352cf37876f9e875bfe3470fc48bf9f
hispanicdevian <iantastream@gmail.com>
2023-12-14 08:22:37 -0800

    New resource project

---
+++ src/main/kotlin/MainScreen.kt
@@ -0,0 +1,13 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+@Preview
+fun mainScreen () {
+    Box(modifier = Modifier.fillMaxSize()){
+
+    }
+}
