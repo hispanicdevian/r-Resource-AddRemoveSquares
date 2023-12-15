ae83322eaf6e743d51a38c52610b2d2dd7062692
hispanicdevian <iantastream@gmail.com>
2023-12-15 13:16:45 -0800

    New resource project

--- src/main/kotlin/MainScreen.kt
+++ src/main/kotlin/MainScreen.kt
@@ -1,9 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -12,32 +15,46 @@ import androidx.compose.ui.unit.dp
 @Composable
 @Preview
 fun mainScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center) {
+    var squaresCount by remember { mutableStateOf(0) }
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 8.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                squaresCount++
+            },
+            colors = ButtonDefaults.buttonColors(Color.DarkGray)
         ) {
-            Button(
-                onClick = { /* No action */ },
-                colors = ButtonDefaults.buttonColors(Color.DarkGray)
-            ) {
-                Text("Add Square")
+            Text("Add Square")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            modifier = Modifier.padding(start = 16.dp)
+        ) {
+            items((0 until squaresCount).chunked(4)) { chunkedSquares ->
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    chunkedSquares.forEach { _ ->
+                        squaresMS()
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
-        Column(
-            modifier = Modifier
-                .padding(top = 15.dp)
-                .fillMaxSize(1f),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-
-            squares()
-        }
     }
+}
+
+@Composable
+fun squaresMS() {
+    Box(
+        modifier = Modifier
+            .size(50.dp)
+            .background(Color.Blue)
+    )
 }
