5ff01f02fb39b2e6850a00e67cf09bf159f12187
hispanicdevian <iantastream@gmail.com>
2023-12-15 08:44:49 -0800

    New resource project

--- src/main/kotlin/MainScreen.kt
+++ src/main/kotlin/MainScreen.kt
@@ -1,13 +1,43 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview
-fun mainScreen () {
-    Box(modifier = Modifier.fillMaxSize()){
+fun mainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 8.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { /* No action */ },
+                colors = ButtonDefaults.buttonColors(Color.DarkGray)
+            ) {
+                Text("Add Square")
+            }
+        }
+        Column(
+            modifier = Modifier
+                .padding(top = 15.dp)
+                .fillMaxSize(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            squares()
+        }
     }
 }
